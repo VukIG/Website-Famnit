2a5b590813427626017cb552711de0fa44a26d75
VukIG <vuk.ignjatovic05@gmail.com>
2024-12-12 20:35:48 +0100

    Landing page completed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,18 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="bg-white w-full h-full relative font-futura">
+
+  <div 
+    class="absolute bg-black top-0 right-0 z-0 w-full h-full bg-right bg-no-repeat bg-50%"
+    style="background-image: url('/SCLogo.png'); clip-path: polygon(0 0, 100% 100%, 100% 0);">
+  </div>
+
+  <div class="w-[100vw] z-10 h-[100vh] bg-white border-l-[50vw] border-r-[50vw] border-b-[50vh]
+   border-l-white border-b-white border-t-[50vh]">
+
+   <div class="absolute left-[10em] top-[19em] flex justify-center flex-col gap-7">
+      <button class="flex justify-center align-middle w-[7em] p-5 bg-standardColor rounded-xl text-white text-5xl">
+        Log in
+      </button>
+      <p class="text-[#8B8B8B] pl-10">And become a part of the community</p>
+    </div>
+  </div>
+</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,32 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				standardColor: '#191919', // Replace with your color
+			},
+			backgroundImage: {
+				logo: "url('/static/SCLogo.png')",
+			},
+			fontFamily: {
+				futura: ['Futura PT', 'sans-serif']
+			},
+			backgroundSize:{
+				'50%': '60%'
+			}
+		},
 	},
 
-	plugins: [typography, forms, containerQueries]
+	plugins: [
+		typography,
+		forms,
+		containerQueries,
+		function ({ addUtilities }) {
+			addUtilities({
+				'.jebemTiMajku': {
+					'clip-path': 'polygon(0% 0%, 100% 100%)',
+				},
+			});
+		},
+	],
 } satisfies Config;
