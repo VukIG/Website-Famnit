a755af6806a405e79149a0085c0bc1d7bb60deef
VukIG <vuk.ignjatovic05@gmail.com>
2024-12-13 20:20:17 +0100

    Dodat about us

---
+++ src/routes/about-us/+page.svelte
@@ -0,0 +1,23 @@
+
+<script lang="ts">
+    export const data: Record<string, unknown> = {}; // Replace with the actual type for page data if available.
+  </script>
+  
+  <section class="flex items-center justify-center min-h-screen px-8 bg-[#191919] text-white relative">
+    <div class="absolute inset-0 flex flex-col items-center">
+      <h1 class="lg:text-[20vw] leading-none tracking-tight uppercase max-w-full z-[0]">
+        About<br>
+        <span class="block pl-30">Us</span>
+      </h1>
+      
+      <!-- New div for paragraph text -->
+      <div class="absolute right-[15%] top-[55%] text-left w-[40%]">
+        <p class="text-lg text-[#979797]">
+          The Student Council is a vital part of our academic community, serving as a bridge between students and the administration. 
+          Comprising elected representatives, the council embodies the interests and concerns of the student body. 
+          Their main mission is to promote a positive school environment and foster school spirit.
+        </p>
+      </div>
+    </div>
+  </section>
+  
