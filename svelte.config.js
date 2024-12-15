05aa21c75e0755dcafd756dbea14453ef0e2d3f8
VukIG <vuk.ignjatovic05@gmail.com>
2024-12-15 17:51:41 +0100

    LJUDI AJMO VISE DEPLOJUJ SE NISI SPEJSATL

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,9 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			strict:false
+		}),
 	},
 	preprocess: vitePreprocess({
 		postcss: true,
